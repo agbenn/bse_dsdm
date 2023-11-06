a75194fae2e06f740b37937f543e67f359f60b4e
agbenn <hockeystar614@yahoo.com>
2023-11-06 13:01:52 +0100

    add docstrings to outlier methods + rename

--- preprocessing/outlier_detection.py
+++ preprocessing/outlier_detection.py
@@ -5,11 +5,14 @@ from sklearn.covariance import EllipticEnvelope
 from sklearn.neighbors import LocalOutlierFactor
 from sklearn.svm import OneClassSVM
 
-def detect_outliers_iqr(data): 
-    # Detect outliers:
+def remove_outliers_iqr(column_of_data): 
+    '''
+    method removes outliers using an interquartile range
+    :param object column_of_data: a column or series of a dataframe
+    '''
 
     # Calculate quartiles 25% and 75%
-    q25, q75 = np.quantile(data, 0.25), np.quantile(data, 0.75)
+    q25, q75 = np.quantile(column_of_data, 0.25), np.quantile(column_of_data, 0.75)
 
     # calculate the IQR
     iqr = q75 - q25
@@ -21,18 +24,22 @@ def detect_outliers_iqr(data):
     lower, upper = q25 - cut_off, q75 + cut_off
 
     # Calculate the number of records below and above lower and above bound value respectively
-    outliers = [x for x in data if (x >= upper) | (x <= lower)]
+    column_of_data = [x for x in column_of_data if (x <= upper) | (x >= lower)]
 
     # Print basic information (can be removed)
     print('The IQR is',iqr)
     print('The lower bound value is', lower)
     print('The upper bound value is', upper)
 
-    return outliers
+    return column_of_data
 
 
-def detect_outliers_std_deviation(column_of_data, threshold=3):
-    
+def remove_outliers_std_deviation(column_of_data, threshold=3):
+    '''
+    method removes outliers using a standard deviation
+    :param object column_of_data: a column or series of a dataframe
+    :param int threshold: level at which outliers are trimmed by std dev
+    '''
     mean = np.mean(column_of_data)
     std = np.std(column_of_data)
     cutoff = threshold * std
@@ -40,41 +47,58 @@ def detect_outliers_std_deviation(column_of_data, threshold=3):
     upper_bound = mean + cutoff
 
     # Calculate the number of records below and above lower and above bound value respectively
-    outliers = [x for x in column_of_data if (x >= upper_bound) | (x <= lower_bound)]
-
+    column_of_data = [x for x in column_of_data if (x <= upper_bound) | (x >= lower_bound)]
 
     # Print basic information (can be removed)
     print('The mean is',mean)
     print('The std is',std)
     print('The lower bound value is', lower_bound)
     print('The upper bound value is', upper_bound)
-    print('The list of outliers are', outliers)
 
-    return outliers
+    return column_of_data
 
 
-def detect_outliers_iso_forest(column_of_data):
-    iso = IsolationForest(contamination=0.1)
+def remove_outliers_iso_forest(column_of_data, contamination=0.01):
+    '''
+    method removes outliers in using an iso forest 
+    (splits data between max and min values and trims the furthest branches at a contamination level)
+    :param object column_of_data: a column or series of a dataframe
+    :param float contamination: level at which outliers are trimmed (0-0.5)
+    '''
+    iso = IsolationForest(contamination=contamination)
     yhat = iso.fit_predict(column_of_data)
     # select all rows that are not outliers
     mask = yhat != -1
     return column_of_data[mask]
 
-def detect_outliers_min_covariance_det(column_of_data):
-    ee = EllipticEnvelope(contamination=0.01)
+def remove_outliers_min_covariance_det(column_of_data, contamination=0.01):
+    '''
+    removeing outliers based on a Gaussian distribution
+    :param object column_of_data: a column or series of a dataframe
+    :param float contamination: level at which outliers are trimmed (0-0.5)
+    '''
+    ee = EllipticEnvelope(contamination=contamination)
     yhat = ee.fit_predict(column_of_data)
     # select all rows that are not outliers
     mask = yhat != -1
     return column_of_data[mask]
 
-def detect_outliers_local_outlier(column_of_data):
+def remove_outliers_local_outlier(column_of_data):
+    '''
+    removeing outliers based on a neighbor distance from density algorithm
+    :param object column_of_data: a column or series of a dataframe
+    '''
     lof = LocalOutlierFactor()
     yhat = lof.fit_predict(column_of_data)
     # select all rows that are not outliers
     mask = yhat != -1
     return column_of_data[mask]
 
-def detect_outliers_one_class_svm(column_of_data):
+def remove_outliers_one_class_svm(column_of_data):
+    '''
+    removeing outliers based on an unsupervised support vector machine model
+    :param object column_of_data: a column or series of a dataframe
+    '''
     # identify outliers in the training dataset
     ee = OneClassSVM(nu=0.01)
     yhat = ee.fit_predict(column_of_data)
