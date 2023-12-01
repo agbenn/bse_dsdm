e3f927b3b6eda6cf187969f6857f67b3f65ab433
Julia González <139889487+Juliagonzalezf102@users.noreply.github.com>
2023-12-01 11:29:26 +0100

    Update value_imputer.py

--- bse_dsdm/preprocessing/value_imputer.py
+++ bse_dsdm/preprocessing/value_imputer.py
@@ -6,7 +6,8 @@ import matplotlib.pyplot as plt
 
 from sklearn.impute import KNNImputer
 
-def impute_values(df, columns_to_impute,impute_type='constant', impute_constant=None, n_neighbors=2):
+
+def impute_values(df, columns_to_impute, impute_type='constant', impute_constant=None, n_neighbors=2):
     """
     allows for value impution
     options: ffill, bfill, mean, knn, mode
@@ -15,15 +16,15 @@ def impute_values(df, columns_to_impute,impute_type='constant', impute_constant=
 
     if impute_type == 'constant' and impute_constant is not None:
         filled_df = df.fillna(value=impute_constant)
-    elif impute_type == 'constant' and impute_constant is None: 
+    elif impute_type == 'constant' and impute_constant is None:
         print('must input a impute value constant')
-        return 
+        return
     elif impute_type == 'ffill':
         filled_df = df.fillna(method='ffill')
     elif impute_type == 'bfill':
         filled_df = df.fillna(method='bfill')
     elif impute_type == 'mean':
-        filled_df = df.fillna(df.mean())
+        filled_df = df.fillna(df.mean(numeric_only=True))
     elif impute_type == 'mode':
         filled_df = df.fillna(df.mode())
     elif impute_type == 'knn':
@@ -34,22 +35,20 @@ def impute_values(df, columns_to_impute,impute_type='constant', impute_constant=
 
         # Convert the imputed data back to DataFrame
         filled_df = pd.DataFrame(imputed_data, columns=df.columns)
-        filled_df=df.copy()
-        filled_df[columns_to_impute]=imputed_data
+        filled_df = df.copy()
+        filled_df[columns_to_impute] = imputed_data
     else:
         print('invalid method selection')
-    
+
     return filled_df
 
 
 def impute_categorical(df, columns_to_impute):
-    filled_df = df.copy()  
-    
+    filled_df = df.copy()
+
     for col in columns_to_impute:
-               
-        mode_val = df[col].mode().iloc[0]  
-        filled_df[col].fillna(mode_val, inplace=True)  
-    
+
+        mode_val = df[col].mode().iloc[0]
+        filled_df[col].fillna(mode_val, inplace=True)
 
     return filled_df
-
