921efb753f4feb7a1684928e1962b69adf149387
Julia González <139889487+Juliagonzalezf102@users.noreply.github.com>
2023-12-01 11:29:28 +0100

    Update value_encoder.py

--- bse_dsdm/preprocessing/value_encoder.py
+++ bse_dsdm/preprocessing/value_encoder.py
@@ -3,15 +3,15 @@ from sklearn.preprocessing import OneHotEncoder
 from sklearn.preprocessing import LabelEncoder
 
 
-def encode_categorical_columns(df,column=None):
+def encode_categorical_columns(df, column=None):
     """
     nominal or categorical i.e. blue red green
     """
     column_to_encode = df[[column]]
     # Apply one-hot encoding
     encoder = OneHotEncoder(drop='first', sparse=False)
-    
-    encoded_data = encoder.fit_transform(column_to_encode) 
+
+    encoded_data = encoder.fit_transform(column_to_encode)
     # Convert the encoded data back to a dataframe
 
     encoded_data = pd.DataFrame(encoded_data, columns=encoder.get_feature_names_out([column]))
@@ -19,8 +19,7 @@ def encode_categorical_columns(df,column=None):
     return df
 
 
-
-def encode_ordinal_columns(data, columns=None, mapping=None): 
+def encode_ordinal_columns(data, columns=None, mapping=None):
     """
     levels of category or ordinal i.e. low medium high
     """
@@ -37,7 +36,8 @@ def encode_ordinal_columns(data, columns=None, mapping=None):
 
     return data
 
-def target_encode_column(df, categorical_groupby_value, compute_column, compute_type='mean'): 
+
+def target_encode_column(df, categorical_groupby_value, compute_column, compute_type='mean'):
     """ 
     useful when there is a correlation between the categorical variable and the other variable.
     i.e. mean GDP grouped by country
@@ -46,7 +46,7 @@ def target_encode_column(df, categorical_groupby_value, compute_column, compute_
 
     grouped_by_mapping = None
 
-    # Calculating the 
+    # Calculating the
     if compute_type == 'mean':
         grouped_by_mapping = df.groupby(categorical_groupby_value)[compute_column].mean()
     elif compute_type == 'sum':
@@ -57,17 +57,17 @@ def target_encode_column(df, categorical_groupby_value, compute_column, compute_
     # Encoding the categorical variable 'group_by_value' using target encoding
     return df[categorical_groupby_value].map(grouped_by_mapping)
 
+
 def convert_to_age(df, column_name, reference_year):
-    
+
     df[column_name] = pd.to_datetime(df[column_name], errors='coerce')
     df[column_name] = reference_year - df[column_name].dt.year
     df.rename(columns={column_name: 'age'}, inplace=True)
     return df
 
-import pandas as pd
 
 def extract_string(df, column_name):
-   
+
     df[column_name] = df[column_name].str.replace(', ', ',')
     df[column_name] = df[column_name].str.replace(' ', '_')
     df_encoded = df[column_name].str.get_dummies(sep=',')
